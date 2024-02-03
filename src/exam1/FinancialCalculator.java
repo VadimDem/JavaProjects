0a5d2f1b031eb09ca91f65b551368b00fa493451
VadimDem <vagik51@gmail.com>
2024-02-04 00:04:12 +0300

    Update1

--- src/exam1/FinancialCalculator.java
+++ src/exam1/FinancialCalculator.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class FinancialCalculator {
     static int[] Expenses = new int[30];
-    int ExpensesDay;
+    static int ExpensesDay;
+    static int Day;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(
-                "Mеню:\n" +
+                "Меню:\n" +
                         "1 - Ввести расходы за определенный день\n" +
                         "2 - Траты за месяц\n" +
                         "3 - Самая большая сумма расхода за месяц\n" +
+                        "4 - Конвертер валют\n"+
                         "0 – Выход");
         System.out.print("Введите номер пункта меню: ");
         int Choice = scanner.nextInt();
@@ -26,6 +28,10 @@ public class FinancialCalculator {
                 break;
             case 3:
                 Function3();
+                break;
+            case 4:
+                Converter();
+
             case 0:
                 System.out.println("Выход из программы...");
                 System.exit(0);
@@ -33,26 +39,46 @@ public class FinancialCalculator {
         }
     }
 
-    public static void enterExpenses(){
+    public static void enterExpenses() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Выведите день (от 1 до 30)");
-        int Day = scanner.nextInt();
+        System.out.println("Введите день (от 1 до 30)");
+        Day = scanner.nextInt();
+
         if (Day < 1 || Day > 30) {
             System.out.println("Неверный день. Пожалуйста, введите число от 1 до 30.");
             enterExpenses();
             return;
         }
-        else {
-            System.out.println("Введите траты");
-            int ExpensesDay = scanner.nextInt();
-            Expenses[Day-1] = ExpensesDay;
 
+        if (Expenses[Day - 1] > 0) {
+            System.out.println("Сумма уже указана\n" +
+                    "1. Перезаписать сумму\n" +
+                    "2. Оставить предыдущую");
+            System.out.print("Введите номер пункта:");
+            int Choice = scanner.nextInt();
+            switch (Choice){
+                case 1:
+                    setExpenses();
+                case 2:
+                    Function1();
+            }
+
+        } else {
+            setExpenses();
         }
+
+    }
+
+    public static void setExpenses(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите траты");
+        int expensesDay = scanner.nextInt();
+        Expenses[Day - 1] = expensesDay;
         System.out.println("1. Ввести трату за другой день");
         System.out.println("2. Вернуться в меню");
         System.out.print("Введите номер пункта:");
         int Choice = scanner.nextInt();
-        switch (Choice){
+        switch (Choice) {
             case 1:
                 enterExpenses();
             case 2:
@@ -70,8 +96,10 @@ public class FinancialCalculator {
         switch (Choice){
             case 1:
                 enterExpenses();
+                break;
             case 2:
                 main(new String[]{});
+                break;
         }
     }
 
@@ -87,17 +115,16 @@ public class FinancialCalculator {
         switch (Choice){
             case 1:
                 Function2();
+                break;
             case 2:
                 main(new String[]{});
+                break;
         }
     }
 
     public static void Function3(){
-//        for (int i = 0; i < 30; i++){
-//
-//        }
         int maxIndex = 0;
-        int maxExpense = Expenses[0]; // Предполагаем, что максимальное значение находится в первом элементе массива
+        int maxExpense = Expenses[0];
 
         for (int i = 1; i < Expenses.length; i++) {
             if (Expenses[i] > maxExpense) {
@@ -106,7 +133,28 @@ public class FinancialCalculator {
             }
         }
         System.out.println("Максимальная расход за месяц: " + maxExpense + " в день " + (maxIndex + 1));
+    }
+    public static void Converter(){
+//        for (int i = 1; i < 31; i++){
+//            System.out.println(i + " День - " + (Expenses[i-1] / 98.64));
+//
+//        }
+        double sum = 0;
+        for (double num : Expenses) {
+            sum += num;
+        }
+        double total = sum;
+        double Euro = (sum / 98.64);
+        double USD = (sum / 90.66);
+        double CNY = (sum / 12.6);
+        System.out.println("Сумма всех затрат: " + total + " В евро = " + Euro + " В долларах = " + USD + " В Юанях = " + CNY);
+//        int sum = 0;
+//        for (int i = 1; i < 31; i++){
+//            sum += Expenses[i];
+//        }
+//        double total = sum;
+//        System.out.println("Сумма всех чисел: " + total);
+
 
     }
-
 }
